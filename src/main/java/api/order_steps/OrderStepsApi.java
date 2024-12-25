3c070c6e12b10a37f84954083ff8a3836ceee16d
Yana Shmygleva <shmyaniv@mail.ru>
2024-12-25 20:47:39 +0300

    add Ordersteps

---
+++ src/main/java/api/order_steps/OrderStepsApi.java
@@ -0,0 +1,106 @@
+package api.order_steps;
+
+import api.RestApi;
+import constants.EndPoints;
+import io.qameta.allure.Step;
+import io.restassured.response.ValidatableResponse;
+import pojo.Ingredients;
+import pojo.Order;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static random_data.RandomData.RANDOM_HASH;
+
+public class OrderStepsApi extends RestApi {
+
+  @Step("Send GET request to /api/ingredients")
+  public static ValidatableResponse getAllIngredients() {
+    return given()
+            .spec(getBaseSpec())
+            .log().all()
+            .get(EndPoints.INGREDIENTS_DATA_API)
+            .then()
+            .log().all();
+  }
+
+  @Step("Get data about ingredients as list")
+  public Ingredients getAllIngredientsAsList() {
+    return given()
+            .spec(getBaseSpec())
+            .log().all()
+            .get(EndPoints.INGREDIENTS_DATA_API)
+            .body()
+            .as(Ingredients.class);
+  }
+
+  @Step("Add ingredient to the list")
+  public void addIngredientToTheList(Ingredients ingredients, List<String> ingredient) {
+    ingredient.add(ingredients.getData().get(1).get_id());
+    ingredient.add(ingredients.getData().get(2).get_id());
+    ingredient.add(ingredients.getData().get(3).get_id());
+    ingredient.add(ingredients.getData().get(4).get_id());
+    ingredient.add(ingredients.getData().get(5).get_id());
+  }
+
+  @Step("Add ingredient to the list with wrong hash")
+  public void addIngredientToTheListWithWrongHash(Ingredients ingredients, List<String> ingredient) {
+    ingredient.add(ingredients.getData().get(0).get_id().hashCode() + RANDOM_HASH);
+    ingredient.add(ingredients.getData().get(1).get_id().hashCode() + RANDOM_HASH);
+    ingredient.add(ingredients.getData().get(2).get_id().hashCode() + RANDOM_HASH);
+  }
+
+  @Step("Send GET request to /api/orders")
+  public static ValidatableResponse getOrdersByAuthorization(String accessToken) {
+    return given()
+            .spec(getBaseSpec())
+            .header("Authorization", accessToken)
+            .log().all()
+            .get(EndPoints.ORDER_API)
+            .then()
+            .log().all();
+  }
+
+  @Step("Send GET request to /api/orders")
+  public static ValidatableResponse getOrdersWithoutAuthorization() {
+    return given()
+            .spec(getBaseSpec())
+            .log().all()
+            .get(EndPoints.ORDER_API)
+            .then()
+            .log().all();
+  }
+
+  @Step("Send GET request to /api/orders/all")
+  public static ValidatableResponse getAllOrders() {
+    return given()
+            .spec(getBaseSpec())
+            .log().all()
+            .get(EndPoints.ORDER_API + "all")
+            .then()
+            .log().all();
+  }
+
+  @Step("Send POST request to /api/orders")
+  public static ValidatableResponse createOrderByAuthorization(Order order, String accessToken) {
+    return given()
+            .spec(getBaseSpec())
+            .header("Authorization", accessToken)
+            .body(order)
+            .log().all()
+            .post(EndPoints.ORDER_API)
+            .then()
+            .log().all();
+  }
+
+  @Step("Send POST request to /api/orders")
+  public static ValidatableResponse createOrderWithoutAuthorization(Order order) {
+    return given()
+            .spec(getBaseSpec())
+            .body(order)
+            .log().all()
+            .post(EndPoints.ORDER_API)
+            .then()
+            .log().all();
+  }
+}
---
+++ src/main/java/api/order_steps/OrderStepsChecks.java
@@ -0,0 +1,68 @@
+package api.order_steps;
+
+import io.qameta.allure.Step;
+import io.restassured.response.ValidatableResponse;
+import org.hamcrest.CoreMatchers;
+
+import static constants.AnswersWhenChecksDone.ORDER_GET_OR_USER_UPDATE_MESSAGE_UNAUTHORIZED;
+import static constants.AnswersWhenChecksDone.ORDER_MESSAGE_BAD_REQUEST;
+import static org.apache.http.HttpStatus.*;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class OrderStepsChecks {
+
+  @Step("Check status code 200 and body response - success:true when we create order with authorization")
+  public void checkOrderCreateSuccessWithAuthorization(ValidatableResponse response) {
+    response
+            .statusCode(SC_OK)
+            .and().assertThat().body("success", CoreMatchers.equalTo(true))
+            .and().assertThat().body("order.number", notNullValue())
+            .and().assertThat().body("order._id", notNullValue());
+  }
+
+  @Step("Check status code 200 and body response - success:true when we create order without authorization")
+  public void checkOrderCreateSuccessWithoutAuthorization(ValidatableResponse response) {
+    response
+            .statusCode(SC_OK)
+            .and().assertThat().body("success", CoreMatchers.equalTo(true))
+            .and().assertThat().body("order.number", notNullValue());
+  }
+
+  @Step("Check status code 400 and body response - success:false when we create order without ingredients")
+  public void checkOrderCreateWithoutIngredients(ValidatableResponse response) {
+    response
+            .statusCode(SC_BAD_REQUEST)
+            .and().assertThat().body("message", CoreMatchers.equalTo(ORDER_MESSAGE_BAD_REQUEST))
+            .and().assertThat().body("success", CoreMatchers.equalTo(false));
+  }
+
+
+  @Step("Check status code 500 when we create order without authorization and change hash ingredient")
+  public void CheckOrderCreateWithWrongHashIngredient(ValidatableResponse response) {
+    response
+            .statusCode(SC_INTERNAL_SERVER_ERROR);
+  }
+
+  @Step("Check status code 200 and body response - success:true when we get all ingredients of order")
+  public void checkWhenWeGetAllIngredientsOfOrder(ValidatableResponse response) {
+    response
+            .statusCode(SC_OK).log().all()
+            .and().assertThat().body("success", CoreMatchers.equalTo(true));
+  }
+
+  @Step("Check status code 200 and body response - success:true when get all orders")
+  public void checkWhenWeGetAllOrders(ValidatableResponse response) {
+    response
+            .statusCode(SC_OK).log().all()
+            .and().assertThat().body("success", CoreMatchers.equalTo(true));
+  }
+
+  @Step("Check status code 401 and body response when we get order without user authorization")
+  public void checkWhenWeGetOrdersWithoutUserAuthorization(ValidatableResponse response) {
+    response
+            .statusCode(SC_UNAUTHORIZED).log().all()
+            .and().assertThat().body("success", CoreMatchers.equalTo(false))
+            .and().assertThat().body("message", CoreMatchers.equalTo(ORDER_GET_OR_USER_UPDATE_MESSAGE_UNAUTHORIZED)).log().all();
+  }
+
+}
