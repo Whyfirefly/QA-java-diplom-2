350f146e823daadf1f2c5530e3dcb12dd17cd1c0
Yana Shmygleva <shmyaniv@mail.ru>
2024-12-25 20:46:49 +0300

    add constants

---
+++ src/main/java/constants/AnswersWhenChecksDone.java
@@ -0,0 +1,12 @@
+package constants;
+
+public class AnswersWhenChecksDone {
+  public static final String ORDER_MESSAGE_BAD_REQUEST = "Ingredient ids must be provided";
+  public static final String ORDER_GET_OR_USER_UPDATE_MESSAGE_UNAUTHORIZED = "You should be authorised";
+  public static final String USER_MESSAGE_ALREADY_EXIST = "User already exists";
+  public static final String USER_CREATE_MESSAGE_EMPTY_FIELD = "Email, password and name are required fields";
+  public static final String USER_SUCCESSFULLY_REMOVED_MESSAGE = "User successfully removed";
+  public static final String MESSAGE_LOGOUT_SUCCESS = "Successful logout";
+  public static final String LOGIN_MESSAGE_UNAUTHORIZED = "email or password are incorrect";
+}
+
