2a89d0cd4fdf32a5320374fc251b4fbe1c9dabde
Yana Shmygleva <shmyaniv@mail.ru>
2024-12-25 20:48:33 +0300

    add RandomData generators

---
+++ src/main/java/random_data/RandomData.java
@@ -0,0 +1,8 @@
+package random_data;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class RandomData {
+  public static String RANDOM_HASH = RandomStringUtils.randomNumeric(8).toLowerCase();
+
+}
